328d9f743581339469d3204e6fc47345d31788f9
Casey <Kusella-Gussin>
2019-12-10 00:08:44 +0000

    Update 1: We Have Movement
    All the updates up to now were just about basic Asset making but I have finally put together the basics and we have a room and character controller.

---
+++ The Grand Heist/The Grand Heist Unity/Assets/Scripts/Character_Controller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+
+    public Transform character;
+
+    float xRot = 0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursorLock();
+    }
+
+    // Update is called once per frame
+
+
+    private void cursorLock ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+
+    }
+
+    void Update()
+    {
+       cameraRotation();
+        
+
+    }
+
+
+
+    private void cameraRotation()
+    {
+        float movX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float movY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRot -= movY;
+        xRot = Mathf.Clamp(xRot, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        character.Rotate(Vector3.up * movX);
+
+
+    }
+
+
+}
---
+++ The Grand Heist/The Grand Heist Unity/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+    public CharacterController characterController;
+    public float Speed = 12f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+
+    }
+
+    public void playerMovement()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        Vector3 targetArea = transform.right * xAxis + transform.forward * zAxis;
+
+        characterController.Move(targetArea * Speed * Time.deltaTime);
+
+    }
+
+
+}
